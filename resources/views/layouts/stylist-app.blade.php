1329cf276398cb78e36af1f0891c522a81cabee2
Kashan Shaikh <shaikhkashan670@gmail.com>
2026-06-12 01:07:26 +0500

    s

--- resources/views/layouts/stylist-app.blade.php
+++ resources/views/layouts/stylist-app.blade.php
@@ -12,6 +12,7 @@
 <body>
     @yield('content')
     <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     @yield('scripts')
 </body>
 </html>
